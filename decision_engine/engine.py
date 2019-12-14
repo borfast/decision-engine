9fb21ed00846759eae1de61b2b37eef96c7c0995
Raúl Santos <borfast@gmail.com>
2019-12-14 19:47:38 +0000

    Add name to engines so they can be exported in the future

--- decision_engine/engine.py
+++ decision_engine/engine.py
@@ -7,8 +7,9 @@ from decision_engine.rules import Rule
 class Engine:
     rules: List = []
 
-    def __init__(self, rules: List[Rule]) -> None:
+    def __init__(self, rules: List[Rule], name: str = None) -> None:
         self.rules = rules
+        self.name = name
 
     def decide(self, data: dict) -> bool:
         return all([rule.check(data) for rule in self.rules])
